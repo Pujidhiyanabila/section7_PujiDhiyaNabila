8331b729bc32e6554212bbbf679d044261d9718b
Pujidhiyanabila <nabilapuji26@gmail.com>
2022-03-04 17:27:49 +0700

    Section 7 Nomor 1 Terbaru

--- pratikum7/bin/soal1.dart
+++ pratikum7/bin/soal1.dart
@@ -1,65 +1,35 @@
-void main () async {
+Future <List> soal(List listData, int kali){
+  var data = [];
   int i = 0;
-  int kali = 5;
-  List <int> data = [10, 22, 34, 46, 58, 62, 74, 81, 25, 15]; 
-
-  for ( i = 0; i < data.length; i++){
-    print("List Data : ${data[i]}");
-    print("Hasil kali ${data[i]} * $kali adalah ${data[i]*kali}");
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // List <int> kali = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  // print("Perkalian $kali");
-  // for ( i = 0; i < kali.length; i++){
-  //   print("Perkalian : ${kali[i]}");
-  // }
-  
-  // print("Hasil Kali $data[i]*kali");
-
-  // print("hasil kali ${data[0]} * ${kali[0]} adalah ${data[0]*kali[0]}");
-  // print("hasil kali ${data[1]} * ${kali[1]} adalah ${data[1]*kali[1]}");
-  // print("hasil kali ${data[2]} * ${kali[2]} adalah ${data[2]*kali[2]}");
-  // print("hasil kali ${data[3]} * ${kali[3]} adalah ${data[3]*kali[3]}");
-  // print("hasil kali ${data[4]} * ${kali[4]} adalah ${data[4]*kali[4]}");
-  // print("hasil kali ${data[5]} * ${kali[5]} adalah ${data[5]*kali[5]}");
-  // print("hasil kali ${data[6]} * ${kali[6]} adalah ${data[6]*kali[6]}");
-  // print("hasil kali ${data[7]} * ${kali[7]} adalah ${data[7]*kali[7]}");
-  // print("hasil kali ${data[8]} * ${kali[8]} adalah ${data[8]*kali[8]}");
-  // print("hasil kali ${data[9]} * ${kali[9]} adalah ${data[9]*kali[9]}");
+  do {
+    int perkalian = listData[i] * kali;
+    data.add(perkalian);
+    i++;
+  } while (i < listData.length);
+  return Future.delayed(Duration(seconds: 1), (){
+    return data;
+  });
 }
 
+void main () async {
+  int kali = 5;
+  var list = [10, 22, 34, 46, 58, 62, 74, 81, 25, 15];
+  var newList = await soal(list, kali);
 
+  print(list.length);
+  print(list);
+  print(newList.length);
+  print(newList);
+}
 
+///////////////////////////////////////////////////////////////////////////////
 
-// void listData () async{
-//   var scores = [12, 20, 6, 8, 5];
-//   print("List Data $scores");
-// }
+// void main () async {
+//   int i = 0;
+//   int kali = 5;
+//   List <int> data = [10, 22, 34, 46, 58, 62, 74, 81, 25, 15]; 
 
-
-// void perkalian (){
-//   var kali = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-//   print("Perkalian $kali");
-// }
-
-// void main (){
-//   perkalian();
-//   listData();
-
-//   print("Hasil kali ${scores[1]*kali[1]}");
-
-// }
+//   for ( i = 0; i < data.length; i++){
+//     print("List Data : ${data[i]}");
+//     print("Hasil kali ${data[i]} * $kali adalah ${data[i]*kali}");
+//   }
